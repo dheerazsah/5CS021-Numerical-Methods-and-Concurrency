3e6a090e10231edd349d355fabb33c26f78b188e
dheerazsah <83595627+dheerazsah@users.noreply.github.com>
2023-02-19 18:02:12 +0545

    Q1.c

---
+++ Week 1 - Variables, Datatypes, Conditions, and Loops/Workshop/Q1.c
@@ -0,0 +1,12 @@
+//1. Write a program to find the sum of numbers from 1 to 10.
+#include <stdio.h>
+int main(){
+    int i;
+    int sum;
+    for(i=1; i<=10; i++)
+    sum=sum+i;
+    {
+    printf("%d ",sum);
+    }
+return 0;
+}
