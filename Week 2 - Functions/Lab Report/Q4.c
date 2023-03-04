52ae77463f6d443e2715a332f6611d45143ce141
dheerazsah <83595627+dheerazsah@users.noreply.github.com>
2023-03-04 20:20:59 +0545

    Q4.c pushed

---
+++ Week 2 - Functions/Lab Report/Q4.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void check_armstrong(){
+    int num, r, add = 0, numO;
+    printf("Enter a numbern (Check Armstrong): ");
+    scanf("%d", &num);
+    numO = num;
+
+    while (num != 0){
+        r = num  % 10;
+        add += r*r*r;
+        num /= 10;
+    }
+    if (numO == add){
+        printf("%d is an armstrong number.\n", numO);
+    } 
+    else {
+        printf("%d is not an armstrong number.\n", numO);
+    }
+}
+
+void check_perfect(){
+    int num, r, add = 0;
+    printf("Enter a number: (Check Perfect)");
+    scanf("%d", &num);
+
+    for (int i = 1; i < num; i++){
+      r = num % i;
+      if (num % i == 0){
+         add += i;
+      }
+    }
+    
+    if ( add == num){
+      printf("%d is a perfect number.\n", num);
+    }
+    else {
+      printf("%d is not a perfect number.\n", num);
+    }
+}
+
+void main(){
+    check_armstrong();
+    check_perfect();
+}
